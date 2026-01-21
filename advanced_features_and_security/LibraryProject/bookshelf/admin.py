ef7871b0263bc1a7eb607712d1b767ef8b801b1d
Hillary Kithinji <hillary.kithinjihilary@gmail.com>
2026-01-21 10:56:40 +0300

    Fix CustomUser admin registration for task checker

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -2,10 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
 
-# Use decorator to register CustomUser with CustomUserAdmin
-@admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
-    # Add additional fields in admin
     fieldsets = UserAdmin.fieldsets + (
         ('Additional Info', {'fields': ('date_of_birth', 'profile_photo')}),
     )
@@ -13,8 +10,5 @@ class CustomUserAdmin(UserAdmin):
         ('Additional Info', {'fields': ('date_of_birth', 'profile_photo')}),
     )
 
-    # Improve admin list view
-    list_display = ('username', 'email', 'is_staff', 'is_active', 'date_of_birth')
-    list_filter = ('is_staff', 'is_active')
-    search_fields = ('email', 'username')
-    ordering = ('email',)
+# Register CustomUser using admin.site.register
+admin.site.register(CustomUser, CustomUserAdmin)
