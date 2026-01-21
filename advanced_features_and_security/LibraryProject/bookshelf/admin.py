2fd9059ef0d8a0b4a0710f7658bca0e0bd0c4daf
Hillary Kithinji <hillary.kithinjihilary@gmail.com>
2026-01-21 10:51:15 +0300

    Add advanced_features_and_security project with CustomUser implementation

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -2,11 +2,19 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
 
+# Use decorator to register CustomUser with CustomUserAdmin
 @admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
+    # Add additional fields in admin
     fieldsets = UserAdmin.fieldsets + (
         ('Additional Info', {'fields': ('date_of_birth', 'profile_photo')}),
     )
     add_fieldsets = UserAdmin.add_fieldsets + (
         ('Additional Info', {'fields': ('date_of_birth', 'profile_photo')}),
     )
+
+    # Improve admin list view
+    list_display = ('username', 'email', 'is_staff', 'is_active', 'date_of_birth')
+    list_filter = ('is_staff', 'is_active')
+    search_fields = ('email', 'username')
+    ordering = ('email',)
