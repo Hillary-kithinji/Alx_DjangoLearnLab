ca8c5c606163c4f95fda83aaa96d4c91c70faac5
Hillary Kithinji <hillary.kithinjihilary@gmail.com>
2026-02-11 10:49:24 +0300

    Fix CRUD views latest

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,12 +1,62 @@
+# blog/views.py
+
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from .models import Post
+from .forms import PostForm
+
+# List all posts
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/post_list.html'
+    context_object_name = 'posts'
+    ordering = ['-published_date']
+
+# View a single post
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
+
+# Create a post
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'blog/post_form.html'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+# Update a post
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'blog/post_form.html'
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+
+# Delete a post
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    template_name = 'blog/post_confirm_delete.html'
+    success_url = '/posts/'
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+
+# ------------------------
+# Authentication views below
+# ------------------------
 from django.shortcuts import render, redirect
-from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth import login, logout
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib import messages
 from .forms import RegisterForm
-from django.contrib.auth.forms import AuthenticationForm
-from django.contrib.auth.models import User
 
-# Registration view
 def register_view(request):
     if request.method == 'POST':
         form = RegisterForm(request.POST)
@@ -16,12 +66,11 @@ def register_view(request):
             messages.success(request, "Registration successful!")
             return redirect('home')
         else:
-            messages.error(request, "Registration failed. Please correct the errors below.")
+            messages.error(request, "Registration failed. Correct errors below.")
     else:
         form = RegisterForm()
     return render(request, 'blog/register.html', {'form': form})
 
-# Login view
 def login_view(request):
     if request.method == 'POST':
         form = AuthenticationForm(request, data=request.POST)
@@ -36,18 +85,15 @@ def login_view(request):
         form = AuthenticationForm()
     return render(request, 'blog/login.html', {'form': form})
 
-# Logout view
 def logout_view(request):
     logout(request)
     messages.success(request, "You have been logged out.")
     return redirect('home')
 
-# Profile view
 @login_required
 def profile_view(request):
     if request.method == 'POST':
-        email = request.POST.get('email')
-        request.user.email = email
+        request.user.email = request.POST.get('email')
         request.user.save()
         messages.success(request, "Profile updated successfully!")
     return render(request, 'blog/profile.html', {'user': request.user})
