c422baa7dbbd16182edec9ae2a4d4f61fd38cf5c
Hillary Kithinji <hillary.kithinjihilary@gmail.com>
2026-02-11 11:36:21 +0300

    update on tags in forms.py

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -4,17 +4,21 @@ from django.contrib.auth.models import User
 from .models import Post
 from .models import Comment
 from taggit.forms import TagWidget  # <- for tag input
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'content', 'tags']  # include tags
+        widgets = {
+            'tags': TagWidget(),  # <-- this makes tags editable in the form
+        }
+
 class RegisterForm(UserCreationForm):
     email = forms.EmailField(required=True)
 
     class Meta:
         model = User
         fields = ["username", "email", "password1", "password2"]
-
-class PostForm(forms.ModelForm):
-    class Meta:
-        model = Post
-        fields = ['title', 'content']
 
 class CommentForm(forms.ModelForm):
     class Meta:
