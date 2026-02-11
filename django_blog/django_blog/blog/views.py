ea048b5f97300a2bc76aba9493c46af5b635d5ba
Hillary Kithinji <hillary.kithinjihilary@gmail.com>
2026-02-11 10:43:36 +0300

    views update

--- django_blog/django_blog/blog/views.py
+++ django_blog/django_blog/blog/views.py
@@ -1,17 +1,17 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth import login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from .forms import RegisterForm
-from django.contrib.auth.forms import AuthenticationForm
-from django.contrib.auth.models import User
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.forms import AuthenticationForm
+from .forms import RegisterForm, PostForm
 from .models import Post
-from .forms import PostForm
 
-# Registration view
+# ------------------------
+# Authentication Views
+# ------------------------
 def register_view(request):
     if request.method == 'POST':
         form = RegisterForm(request.POST)
@@ -26,7 +26,7 @@ def register_view(request):
         form = RegisterForm()
     return render(request, 'blog/register.html', {'form': form})
 
-# Login view
+
 def login_view(request):
     if request.method == 'POST':
         form = AuthenticationForm(request, data=request.POST)
@@ -41,13 +41,13 @@ def login_view(request):
         form = AuthenticationForm()
     return render(request, 'blog/login.html', {'form': form})
 
-# Logout view
+
 def logout_view(request):
     logout(request)
     messages.success(request, "You have been logged out.")
     return redirect('home')
 
-# Profile view
+
 @login_required
 def profile_view(request):
     if request.method == 'POST':
@@ -56,19 +56,23 @@ def profile_view(request):
         request.user.save()
         messages.success(request, "Profile updated successfully!")
     return render(request, 'blog/profile.html', {'user': request.user})
-# List all posts (accessible to all users)
+
+
+# ------------------------
+# Blog Post CRUD Views
+# ------------------------
 class PostListView(ListView):
     model = Post
     template_name = 'blog/post_list.html'
     context_object_name = 'posts'
-    ordering = ['-published_date']  # newest first
+    ordering = ['-published_date']
 
-# View a single post (accessible to all users)
+
 class PostDetailView(DetailView):
     model = Post
     template_name = 'blog/post_detail.html'
 
-# Create a post (authenticated users only)
+
 class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
     form_class = PostForm
@@ -78,7 +82,7 @@ class PostCreateView(LoginRequiredMixin, CreateView):
         form.instance.author = self.request.user
         return super().form_valid(form)
 
-# Update a post (author only)
+
 class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
     form_class = PostForm
@@ -88,7 +92,7 @@ class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
         post = self.get_object()
         return self.request.user == post.author
 
-# Delete a post (author only)
+
 class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Post
     template_name = 'blog/post_confirm_delete.html'
